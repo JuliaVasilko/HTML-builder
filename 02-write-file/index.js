1affab94b24b589d9233dde4f0829f55edef063e
Julia Vasilko <juliavasilko2015@gmail.com>
2025-01-18 21:30:00 +0200

    fix: fix some according to check criteria

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -7,8 +7,21 @@ const writeStream = fs.createWriteStream(dirPath);
 writeStream.on('open', () => {
   console.log('Write your text: ');
 });
-writeStream.on('data', (chunk) => {
-  writeStream.write(chunk);
+
+process.stdin.on('data', (data) => {
+  const input = data.toString().trim();
+
+  if (input.toLowerCase() === 'exit') {
+    console.log('Goodbye!');
+    process.exit(0);
+  }
+
+  writeStream.write(`${input}\n`);
+});
+
+process.addListener('SIGINT', () => {
+  console.log('Goodbye!');
+  process.exit(0);
 });
 
 process.stdin.pipe(writeStream);
